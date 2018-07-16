54e41484870c90c31cb84b1d8766779243970554
Nancy Jo <enciie@gmail.com>
2018-07-16 06:56:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,7 +27,11 @@ class Dog
   def save
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed:breed)
   end
 
   def self.find_by_name
